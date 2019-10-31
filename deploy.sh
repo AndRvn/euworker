d3718acce4036bf3159b864482f84b44913a36cd
Andrey Sereda <andreysereda404@gmail.com>
2019-10-31 15:28:39 +0200

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,45 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "pull master..."
+ESC_SEQ="\033["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+CELL_LINE="========================================="
+CELL_BEGIN="\n$COL_MAGENTA $CELL_LINE \n"
+CELL_DONE_BEGIN="\n$COL_GREEN $CELL_LINE \n"
+CELL_END="\n $CELL_LINE $COL_RESET \n"
+
+echo "$CELL_BEGIN ============= Pull master ============== $CELL_END"
 git pull origin master
 
 echo ""
-echo "composer install..."
+echo "Composer install..."
 composer install
+
+echo "$CELL_BEGIN =========== Install/update composer dependecies ============ $CELL_END"
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+echo "$CELL_BEGIN =========== Run database migrations ============ $CELL_END"
+php artisan migrate --force
+
+echo "$CELL_BEGIN =========== Clear caches ============ $CELL_END"
+php artisan cache:clear
+
+echo "$CELL_BEGIN =========== Generate JWT ============ $CELL_END"
+php artisan jwt:secret
+
+echo "$CELL_BEGIN =========== Clear and cache routes ============ $CELL_END"
+php artisan route:clear
+php artisan route:cache
+
+echo "$CELL_BEGIN =========== Clear and cache config ============ $CELL_END"
+php artisan config:clear
+php artisan config:cache
+
+echo "$CELL_BEGIN =========== Install node modules ============ $CELL_END"
+npm install
+
+echo "$CELL_BEGIN =========== Build assets using Laravel Mix ============ $CELL_END"
+npm run production
+
+echo "$CELL_DONE_BEGIN ================= DONE ================== $CELL_END"
